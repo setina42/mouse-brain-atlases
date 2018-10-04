7df050c9918cea32bab0479bb7e68f5405c03916
setina42 <tina.seg@bluewin.ch>
2018-10-04 11:37:16 +0000

    Flags set

--- make_archives.sh
+++ make_archives.sh
@@ -5,20 +5,19 @@
 #Handle every case correctly:
 #Looks sort of ugly so far....
 MAKE_MESH=false
-while [ ! $# -eq 0 ]
-do 
-     case "$1" in
-          --make-mesh)
-               MAKE_MESH=true
-               shift
 
-     esac
-               PN=$1
-               shift
-               PV=$2
-               shift
-               :
+if [ "$1" == "--make_mesh" ]; then
+     MAKE_MESH=true
+     PN=$2
+     PV=$3
 
+else 
+     PN=$1
+     PV=$2
+     if [ "$3" == "--make_mesh" ]; then
+          MAKE_MESH=true
+     fi
+fi
 
 if [ -z "$PV" ]; then
      PV=9999
@@ -40,16 +39,16 @@ pushd ${P}
      cp ambmc_COPYING ../${PHD}
      cp ambmc_README ../${PHD}
      bash ../dsurqec.sh || exit 1
-     bash ../get_abi_atlases.sh || exit 1
-     python ../nrrd_to_nifti.py || exit 1
-     bash ../ants_reg.sh || exit 1     
+     bash ../abi.sh || exit 1
+     bash ../abi2dsurqec_40micron.sh || exit 1     
      rm abi_10_average.nii.gz 
      rm abi_10_annotation.nii.gz
      mv abi_15_average.nii.gz ../${PHD}
      mv abi_15_annotation.nii.gz ../${PHD}
      if [$MAKE_MESH]; then
-          bash ../ambmc2dsurqec.sh
+          bash ../ambmc2dsurqec.sh || exit 1
           mv ambmc2dsurqec_15micron.nii.gz ../{$PHD}
+          mv ambmc2dsurqec_15micron.obj ../{PHD}
 popd
 tar cfJ "${P}.tar.xz" ${P}
 tar cfJ "${PHD}.tar.xz" ${PHD}
